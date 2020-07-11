475b4e9acc4050765392965c4218026e2b07683e
miysasama <t.kato@creativegear.jp>
2020-07-11 19:16:05 +0900

    Reset CSSを適用

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components/native';
+import { createGlobalStyle, GlobalStyleComponent } from 'styled-components';
 import { useColorScheme } from 'react-native-appearance';
+
+const GlobalStyle = createGlobalStyle`
+  /* CSS Remedy */
+  *,::after,::before{box-sizing:border-box}html{line-sizing:normal}body{margin:0}h1{font-size:2rem}h2{font-size:1.5rem}h3{font-size:1.17rem}h4{font-size:1rem}h5{font-size:.83rem}h6{font-size:.67rem}h1{margin:.67em 0}pre{white-space:pre-wrap}hr{border-style:solid;border-width:1px 0 0;color:inherit;height:0;overflow:visible}audio,canvas,embed,iframe,img,object,svg,video{display:block;vertical-align:middle;max-width:100%}canvas,img,svg,video{height:auto}audio{width:100%}img{border-style:none}svg{overflow:hidden}article,aside,figcaption,figure,footer,header,hgroup,main,nav,section{display:block}[type=checkbox],[type=radio]{box-sizing:border-box;padding:0}
+`;
 
 export interface IThemePart {
   colors: {
@@ -46,17 +52,17 @@ interface IThemeProps {
 const Theme: React.FC<IThemeProps> = (props) => {
   const colorScheme = useColorScheme();
   const isDarkMode = colorScheme === 'dark';
+  const theme =
+    props.theme != undefined && props.theme != null
+      ? isDarkMode
+        ? props.theme.dark
+        : props.theme.light
+      : isDarkMode
+      ? defaultTheme.dark
+      : defaultTheme.light;
   return (
-    <ThemeProvider
-      theme={
-        props.theme != undefined && props.theme != null
-          ? isDarkMode
-            ? props.theme.dark
-            : props.theme.light
-          : isDarkMode
-          ? defaultTheme.dark
-          : defaultTheme.light
-      }>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle theme={theme} />
       {props.children}
     </ThemeProvider>
   );
